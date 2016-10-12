c06e4fae8755acf283a3fa5a350ec0cea3948957
4EverStudent <mail@4everstudent.com>
2016-10-12 12:04:54 +0100

    .

--- app/src/main/java/a4everstudent/downloadingwebcontent/MainActivity.java
+++ app/src/main/java/a4everstudent/downloadingwebcontent/MainActivity.java
@@ -1,10 +1,14 @@
 package a4everstudent.downloadingwebcontent;
 
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,9 +16,32 @@ public class MainActivity extends AppCompatActivity {
     public class DownloadTask extends AsyncTask<String, Void, String>{
 
         @Override
-        protected String doInBackground(String... strings) {
-            Log.i("URL", strings[0]);
-            return null;
+        protected String doInBackground(String... urls) {
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try{
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                int data = reader.read();
+
+                while(data != -1){
+                    char current = (char) data;
+                    result += current;
+
+                    data = reader.read();
+                }
+                return result;
+
+            }
+            catch (Exception e){
+                e.printStackTrace();
+                return "Failed";
+            }
+
         }
     }
     @Override
